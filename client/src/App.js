491ac147a35e5959b019d14a7fc7576ec0704a4d
Daniel Ajayi <danielajayi@C02GM145MD6M.tld>
2024-02-16 23:46:17 -0800

    pivoted from next to MERN without mongoDB

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get("http://localhost:8080").then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data);
+  });
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={apiCall}>Make API Call</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
